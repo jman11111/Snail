6e454527bd6b97dca3ec2e5a485e4eeaa68b652d
jman11111 <jbroncato@yahoo.com>
2020-11-05 15:16:56 -0600

    implemented average finding and ids of CompSci women

--- src/main/java/com/TopBloc/CodingChallenge/ChallengeMain.java
+++ src/main/java/com/TopBloc/CodingChallenge/ChallengeMain.java
@@ -1,5 +1,6 @@
 package com.TopBloc.CodingChallenge;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +9,7 @@ import com.TopBloc.CodingChallenge.Domain.Student;
 
 public class ChallengeMain {
 	
+	//Gets average Score of all students
 	public static int getAverageScore(HashMap<String, Student> students) {
 		int avgScore = 0;
 		for (Map.Entry mapElement : students.entrySet()) {  
@@ -15,8 +17,29 @@ public class ChallengeMain {
             int score = (((Student)mapElement.getValue()).getHighestScore()); 
             avgScore += score;
         } 
+		System.out.println(avgScore);
 		avgScore = avgScore/students.size();
 		return avgScore;
+	}
+	
+	//Gets an Array of the String ids of all Female Computer Science Majors
+	public static String[] getCompSciWomen(HashMap<String, Student> students) {
+		ArrayList<String> studentIdStrings = new ArrayList<String>();
+		for (Map.Entry mapElement : students.entrySet()) {  
+			Student s = ((Student)mapElement.getValue());
+            //get students with Gender of Female and Majoring in Computer Science
+            if(s.getGender() == Student.Gender.F && s.getMajor().toLowerCase().equals("computer science")) {
+            	studentIdStrings.add(s.getStudentId());
+            }
+        } 
+		//convert to array
+		String[] Ids = new String[studentIdStrings.size()];
+		Ids = studentIdStrings.toArray(Ids);
+		return Ids;
+	}
+	
+	public static void sendResults(int avg, String[] ids) {
+		
 	}
 	
 	public static void main(String[] args) {
@@ -24,5 +47,9 @@ public class ChallengeMain {
 		db.createDB();
 		HashMap<String, Student> allStudentsHashMap = db.getAllStudents();
 		System.out.println(getAverageScore(allStudentsHashMap));
+		String[] studentIds = getCompSciWomen(allStudentsHashMap);
+		for(String s : studentIds) {
+			System.out.println(s);
+		}
 	}
 }
